81e10dde6237718856c55373386eeb2d7f1681ce
sunkai <2360801328@qq.com>
2023-02-15 11:33:17 +0800

    1

--- chapter04/src/main/java/com/example/chapter04/StartActivity.java
+++ chapter04/src/main/java/com/example/chapter04/StartActivity.java
@@ -4,15 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class StartActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private  static  final  String TAG ="ning";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        Log.d(TAG,"StartActivity onCreate");
         Button button = findViewById(R.id.btn_activity_jump);
         button.setOnClickListener(this);
     }
@@ -20,5 +23,41 @@ public class StartActivity extends AppCompatActivity implements View.OnClickList
     @Override
     public void onClick(View view) {
         startActivity(new Intent(this,FinshActivity.class));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"StartActivity onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"StartActivity onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"StartActivity onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"StartActivity onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"StartActivity onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG,"StartActivity onRestart");
     }
 }
